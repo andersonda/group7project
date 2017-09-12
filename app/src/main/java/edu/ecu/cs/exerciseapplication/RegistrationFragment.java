b3943dc1460e24150c6039e66f84b4b8e4e8d117
David Anderson <andersonda12@students.ecu.edu>
2017-09-12 15:14:47 -0400

    added registration fragment

---
+++ app/src/main/java/edu/ecu/cs/exerciseapplication/RegistrationFragment.java
@@ -0,0 +1,48 @@
+package edu.ecu.cs.exerciseapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+/**
+ * Created by danderson on 9/12/17.
+ */
+
+public class RegistrationFragment extends Fragment {
+
+    private User mUser;
+
+    private EditText mEditFname, mEditLname, mEditAge, mEditWeight, mEditHeight;
+    private Switch mSwitchWeight, mSwitchHeight, mSwitchSex;
+    private Button mSubmitButton;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+
+        mEditFname = v.findViewById(R.id.register_fname);
+        mEditLname = v.findViewById(R.id.register_lname);
+        mEditAge = v.findViewById(R.id.register_age);
+        mEditWeight = v.findViewById(R.id.register_weight);
+        mEditHeight = v.findViewById(R.id.register_height);
+
+        mSwitchHeight = v.findViewById(R.id.register_height_switch);
+        mSwitchWeight = v.findViewById(R.id.register_weight_switch);
+        mSwitchSex = v.findViewById(R.id.register_sex_switch);
+        mSubmitButton = v.findViewById(R.id.register_submit);
+        return v;
+    }
+
+}
