a8b74a4dc0d4eb28e52af4097d269571d144a2bf
macchelzea <akintades16@students.ecu.edu>
2017-09-21 15:25:27 -0400

    WorkoutActivity created.

--- app/src/main/java/edu/ecu/cs/exerciseapplication/WorkoutFragment.java
+++ app/src/main/java/edu/ecu/cs/exerciseapplication/WorkoutFragment.java
@@ -1,5 +1,6 @@
 package edu.ecu.cs.exerciseapplication;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,12 @@ import android.view.ViewGroup;
  */
 
 public class WorkoutFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
