63bddf0a4cbb636b42f1d183dac2e2049005f4c5
David Anderson <andersonda12@students.ecu.edu>
2017-10-26 10:36:22 -0400

    application now decides whether to start with the registration activity or profile activity depending on if a user exists in the database

--- app/src/main/java/edu/ecu/cs/exerciseapplication/RegistrationActivity.java
+++ app/src/main/java/edu/ecu/cs/exerciseapplication/RegistrationActivity.java
@@ -1,10 +1,28 @@
 package edu.ecu.cs.exerciseapplication;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 public class RegistrationActivity extends SingleFragmentActivity {
     @Override
     public Fragment createFragment(){
         return new RegistrationFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SQLiteDatabase mDatabase = new ExerciseDBHelper(getApplicationContext()).getReadableDatabase();
+        try (UserCursorWrapper cursorWrapper = UserCursorWrapper.queryUser(mDatabase)) {
+            if(cursorWrapper.getCount() == 1){
+                Intent intent = ProfileActivity.newIntent(getApplicationContext());
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 }
